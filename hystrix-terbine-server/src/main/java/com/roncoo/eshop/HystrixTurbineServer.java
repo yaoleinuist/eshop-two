e0475967159d0fb616957df685c641978fff2648
yaolei <39005140+yaoleinuist@users.noreply.github.com>
2019-05-22 16:09:34 +0800

    Add files via upload

---
+++ hystrix-terbine-server/src/main/java/com/roncoo/eshop/HystrixTurbineServer.java
@@ -0,0 +1,15 @@
+package com.roncoo.eshop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class HystrixTurbineServer {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixTurbineServer.class).web(true).run(args);
+    }
+	
+}
---
+++ sleuth-server/src/main/java/com/roncoo/eshop/SleuthServer.java
@@ -0,0 +1,16 @@
+package com.roncoo.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class SleuthServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthServer.class, args);
+    }
+	
+}
